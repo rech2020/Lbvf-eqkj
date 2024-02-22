41700da52d56e9634f27cf1cf312e73ff526d900
Vova41 <nise.shots41@gmail.com>
2024-02-22 18:32:12 +1200

    Commit1

--- Словарь_Ударений.py
+++ Словарь_Ударений.py
@@ -1,26 +1,30 @@
 from itertools import permutations
-from random import *
 
 
+def hash_key(value):
+    return int(''.join(str(ord(c)) for c in value)[2:])
 
-words = []
+
+correct_words_list = []
 correct_words_set = set()
+correct_words_hashes = []
 
-with open('Ударения.txt', encoding="utf-8") as f:
+with open('Ударения.txt', mode='r', encoding="utf-8") as f:
     for word in f:
-        words.append(''.join(word.split()))
+        correct_words_list.append(''.join(word.split()))
         correct_words_set.add(''.join(word.split()))
-correct_words = {key: value for value, key in enumerate(sorted(words), 1001)}
-
-
+        correct_words_hashes.append(str(hash_key(word)))
+true_list = [True]*len(correct_words_list)
+correct_words = {k: v for k, v in zip(correct_words_hashes, zip(correct_words_list, true_list))}
 
 
 vowels_const = set("АЕЁИОУЫЭЮЯаеёиоуыэюя")
-words = set()
+all_words_list = []
 all_words_set = set()
+all_words_hashes = []
 
-for word in correct_words.keys():
-    word = list(word)
+for word in correct_words.values():
+    word = list(word[0])
     count = 0
     for letter in word:
         if letter in vowels_const:
@@ -35,21 +39,27 @@ for word in correct_words.keys():
                 if permutation[count] == '2':
                     word[i] = word[i].upper()
                 count += 1
-        words.add(''.join(word))
+        all_words_list.append(''.join(word))
+        all_words_hashes.append(hash_key(''.join(word)))
         all_words_set.add(''.join(word))
-all_words = {key: value for value, key in enumerate(sorted(words), 1)}
-
-
 
 incorrect_words_set = all_words_set - correct_words_set
-incorrect_words = {key: value for value, key in enumerate(sorted(incorrect_words_set), 2001)}
+incorrect_words_hashes = [hash_key(word) for word in sorted(incorrect_words_set)]
+false_list = [False]*len(incorrect_words_set)
+incorrect_words = {k: v for k, v in zip(incorrect_words_hashes, zip(sorted(incorrect_words_set), false_list))}
+all_words = correct_words | incorrect_words
+
+'''Вывод
 print(all_words)
+print(len(all_words))
 print(correct_words)
+print(len(correct_words))
 print(incorrect_words)
+print(len(incorrect_words))'''
 
 
-
-
+'''
+from random import *
 for i in range(1,20):
     problem = sample(list(all_words_set), 5)
     count = 0
@@ -68,7 +78,7 @@ for i in range(1,20):
                     a.writelines(str(correct_words[word])+'\n')
                 if word in incorrect_words:
                     a.writelines(str(incorrect_words[word])+'\n')
-#print(base)
+print(base)'''
         
     
 
